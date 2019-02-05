be1be2baf3d2184815cdeb22a05dc6cb5bc1f0c0
Daniel Pendolino <daniel@counterhack.com>
2019-02-04 22:11:15 -0500

    add slacktee message at low temp

--- forecast.sh
+++ forecast.sh
@@ -9,14 +9,20 @@ FORECAST_JSON=$(curl $FORECAST_URL 2> /dev/null)
 MAX_DAYS=13
 
 # https://unix.stackexchange.com/questions/13731/is-there-a-way-to-get-the-min-max-median-and-average-of-a-list-of-numbers-in
-for day in $(seq 1 $MAX_DAYS); do
+AVG=$(for day in $(seq 1 $MAX_DAYS); do
   echo $FORECAST_JSON | jq -r --arg day $day '.properties.periods[$day | tonumber].temperature'
-done | jq -s add/length
+done | jq -s add/length)
 
-for day in $(seq 1 $MAX_DAYS); do
+MIN=$(for day in $(seq 1 $MAX_DAYS); do
   echo $FORECAST_JSON | jq -r --arg day $day '.properties.periods[$day | tonumber].temperature'
-done | jq -s min
+done | jq -s min)
 
-for day in $(seq 1 $MAX_DAYS); do
+MAX=$(for day in $(seq 1 $MAX_DAYS); do
   echo $FORECAST_JSON | jq -r --arg day $day '.properties.periods[$day | tonumber].temperature'
-done | jq -s max
+done | jq -s max)
+
+echo $MIN
+
+if [ "$MIN" -le "25" ]; then
+  echo "PIPE FREEZE WARNING!" | slacktee.sh -a "danger" -e "Date and Time" "$(date)" -s "Low Temp" "$MIN" -c "@daniel"
+fi
