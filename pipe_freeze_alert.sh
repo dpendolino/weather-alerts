7abd39eafadd794a6ba4b30098d1d65fef96c5ac
Daniel Pendolino <daniel@counterhack.com>
2019-02-06 19:07:56 -0500

    add usage and split into two scripts

--- pipe_freeze_alert.sh
+++ pipe_freeze_alert.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function usage() {
+  echo -e "Usage:"
+  echo -e "./pipe_freeze_alert.sh temperature"
+  echo
+  echo -e "Send Slack alert if the temperature over the next 14 days dips below the value given."
+}
+
 if [ ! -f /usr/bin/slacktee.sh ]; then
   echo "Slacktee.sh is missing!"
   exit 1
@@ -9,18 +16,15 @@ FORECAST_JSON=$(curl $FORECAST_URL 2> /dev/null)
 MAX_DAYS=13
 
 # https://unix.stackexchange.com/questions/13731/is-there-a-way-to-get-the-min-max-median-and-average-of-a-list-of-numbers-in
-AVG=$(for day in $(seq 1 $MAX_DAYS); do
-    echo $FORECAST_JSON | jq -r --arg day $day '.properties.periods[$day | tonumber].temperature'
-done | jq -s add/length)
-
 MIN=$(for day in $(seq 1 $MAX_DAYS); do
     echo $FORECAST_JSON | jq -r --arg day $day '.properties.periods[$day | tonumber].temperature'
 done | jq -s min)
 
-MAX=$(for day in $(seq 1 $MAX_DAYS); do
-    echo $FORECAST_JSON | jq -r --arg day $day '.properties.periods[$day | tonumber].temperature'
-done | jq -s max)
+if [ -z "$1" ]; then
+  usage
+  exit 1
+fi
 
-echo "MIN,MAX,AVG: $MIN,$MAX,$AVG"
-
-exit 0
+if [ "$MIN" -le "$1" ]; then
+  echo "PIPE FREEZE WARNING!" | slacktee.sh -a "danger" -e "Date and Time" "$(date)" -s "Low Temp" "$MIN" -c "@daniel"
+fi
